3591f6a30d61b8cd6426c02eb025b141cf6a128f
Aahi Awasthee <aahiawasthee@example.com>
2025-09-07 00:25:59 -0500

    Solved Problems

---
+++ search-2d-matrix-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length; //no of rows
+        int n = matrix[0].length; // no. of cols
+        int r = m - 1;
+        int c = 0;
+
+        while (r >= 0 && c < n) {
+            if (matrix[r][c] == target)
+                return true;
+            else if (matrix[r][c] < target) {
+                c++;
+            } else {
+                r--;
+            }
+        }
+        return false;
+    }
+}
